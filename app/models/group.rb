688a7542b67987c471ab4b8df3ff3fa7043a468a
tsuboyama09 <61174442+tsuboyama09@users.noreply.github.com>
2020-02-20 17:02:17 +0900

    delete

--- app/models/group.rb
+++
@@ -1,4 +0,0 @@
-class Group < ApplicationRecord
-  has_many  :users, though: :user_groups
-  has_many :messages
-end
--- app/models/messaages.rb
+++
@@ -1,5 +0,0 @@
-class Massaage < ApplicationRecord
-  has_many :user_group
-  belongs_to :user
-  belongs_to :group
-end

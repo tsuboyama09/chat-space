7205d01a022a49193b145e2f8de1ddec7ec9a349
tsuboyama09 <61174442+tsuboyama09@users.noreply.github.com>
2020-03-05 17:00:22 +0900

    update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -30,6 +30,7 @@ $(function(){
       alert('error');
     });
   };
+    //$(function(){});の閉じタグの直上(処理の最後)に以下のように追記
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
     setInterval(reloadMessages, 7000);
   }
@@ -91,5 +92,4 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   });
-  //$(function(){});の閉じタグの直上(処理の最後)に以下のように追記
 });
